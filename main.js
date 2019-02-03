c2e5bc6fb8343891c9c7aa5ffdf3a753d8ec6b1f
TatyanaLedkova <47081865+TatyanaLedkova@users.noreply.github.com>
2019-02-03 21:37:43 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let startBtn = document.getElementById("start"),
+    budget = document.querySelectorAll('.budget-value'),
+    dayBudget = document.querySelectorAll('.daybudget-value'),
+    level = document.querySelectorAll('.level-value'),
+    expenses = document.querySelectorAll('.expenses-value'),
+    optionalExpenses = document.querySelectorAll('.optionalexpenses-value'),
+    income = document.querySelectorAll('.income-value'),
+    monthSavings = document.querySelectorAll('.monthsavings-value'),
+    checkbox = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    yearSavings = document.querySelectorAll('.yearsavings-value'),
+    year = document.querySelectorAll('.year-value'),
+    month = document.querySelectorAll('.month-value'),
+    day = document.querySelectorAll('.day-value'),
+    choose = document.querySelector('.choose-income'),
+    buttons = document.getElementsByTagName('button'),
+    expensesItemBtn = document.querySelector('.expenses-item-btn'),
+    optionalExpensesBtn = document.querySelector('.optionalexpenses-btn'),
+    countBudgetBtn = document.querySelector('.count-budget-btn');
+
+console.log(startBtn);
+console.log(budget);
+console.log(dayBudget);
+console.log(level);
+console.log(expenses);
+console.log(optionalExpenses);
+console.log(income);
+console.log(monthSavings);
+console.log(checkbox);
+console.log(sum);
+console.log(percent);
+console.log(yearSavings);
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(choose);
+console.log(buttons);
+console.log(expensesItemBtn);
+console.log(optionalExpensesBtn);
+console.log(countBudgetBtn);
